251b174f3c38bbc9fe38e05cd43b683e29c81b32
Madbros <danielmadbros@hotmail.com>
2015-01-07 19:58:56 +0100

    exit ReadXML if no folders node in file

--- CourseDrawer/clsFolders.vb
+++ CourseDrawer/clsFolders.vb
@@ -28,6 +28,8 @@ Public Class clsFolders
         xmlDoc.Load(file)
         If xmlDoc Is Nothing Then Exit Sub
         xmlNode = xmlDoc.DocumentElement.SelectSingleNode("folders")
+        ''' madbros: exit if no folders node in file
+        If xmlNode Is Nothing Then Exit Sub
         xmlNodeReader = New Xml.XmlNodeReader(xmlNode)
         Do While (xmlNodeReader.Read())
             Select Case xmlNodeReader.NodeType
