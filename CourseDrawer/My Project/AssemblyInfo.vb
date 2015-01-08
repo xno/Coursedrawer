e20b06d5592f1ecbda162c094351832a7f8b53e2
Madbros <danielmadbros@hotmail.com>
2015-01-08 22:21:43 +0100

    4.0.005 stable version (load old and new courses, save in CP4.01 format)

--- CourseDrawer/My Project/AssemblyInfo.vb
+++ CourseDrawer/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.004.0")> 
-<Assembly: AssemblyFileVersion("4.0.004.0")> 
+<Assembly: AssemblyVersion("4.0.005.0")> 
+<Assembly: AssemblyFileVersion("4.0.005.0")> 
